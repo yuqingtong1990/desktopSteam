c67da87a864cc4abd3639846b10599a62e085f2b
yuqingtong <yuqingtong@gmail.com>
2017-10-24 11:31:57 +0800

    [ADD] 修改一处警告

--- src/rtmp/lc_rtmpSend.cpp
+++ src/rtmp/lc_rtmpSend.cpp
@@ -737,7 +737,7 @@ int lc_rtmpsend::SendPacket(unsigned int nPacketType,unsigned char *data,unsigne
 	packet.m_nInfoField2 = m_pRtmp->m_stream_id;  
 	packet.m_nBodySize = size;		 
 	memcpy(packet.m_body,data,size);
-	int nRet;
+	int nRet = FALSE;
 	if (RTMP_IsConnected(m_pRtmp)){  
 		nRet = RTMP_SendPacket(m_pRtmp,&packet,TRUE); /*TRUE为放进发送队列,FALSE是不放进发送队列,直接发送*/
 	}		
